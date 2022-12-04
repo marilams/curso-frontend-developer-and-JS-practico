fdf77f613fd282323df7974a48edaf0c2025aa86
marilams <marilams995@gmail.com>
2022-12-04 16:54:02 -0400

    mobile menu

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const userEmail =document.querySelector('#navbar-email');
-const desktopMenu =document.querySelector('.desktop-menu');
+const userEmail = document.querySelector('#navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu =document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-userEmail.addEventListener('click', () => {
+function inactiveDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
-})
+}
+
+function inactiveMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
+
+
+userEmail.addEventListener('click', inactiveDesktopMenu);
+burguerMenu.addEventListener('click', inactiveMobileMenu);
